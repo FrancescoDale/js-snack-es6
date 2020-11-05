02750751ea6aef7d57446b4ce72ae1227535aa2a
FrancescoDale <fd060862@gmail.com>
2020-11-05 23:36:09 +0100

    array con i nomi

---
+++ jsnack2/main.js
@@ -0,0 +1,33 @@
+// array di nomi. Da adesso prendo l'abitudine di dichiarare le variabili direttamente quando mi servono invece di dichiarle prima e poi usarle
+
+var nameIndex = [
+
+    {
+        'nome' :'Erika',
+    },
+    {
+        'nome' : 'Alessandra',
+    },
+    {
+        'nome' : 'Giulia',
+    },
+    {
+        'nome' : 'Andrea',
+    },
+    {
+        'nome' : 'Simone',
+    },
+    {
+        'nome' : 'Federico',
+    },
+    {
+        'nome' : 'Giuseppe',
+    },
+    {
+        'nome' : 'Francesca',
+    },
+    {
+        'nome' : 'Mattia',
+    },
+
+];
