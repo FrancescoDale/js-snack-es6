bcbe5bae97b05638cbea43dba0d737a5a39b3d5c
FrancescoDale <fd060862@gmail.com>
2020-11-05 23:36:50 +0100

    ciclo foreach con le condizioni per la creazione del nuovo array

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,6 +1,6 @@
 // array di nomi. Da adesso prendo l'abitudine di dichiarare le variabili direttamente quando mi servono invece di dichiarle prima e poi usarle
 
-var nameIndex = [
+const nameIndex = [
 
     {
         'nome' :'Erika',
@@ -31,3 +31,22 @@ var nameIndex = [
     },
 
 ];
+
+//chiedo all'utente di inserire due numeri interi compresi tra 0 e numero di oggetti dell'array ( in questo caso è 9 )
+
+const limiteInf = parseInt(prompt('Inserire il limite inferiore ( numeri da 0 a 9 ) : '));
+
+const limiteSup = parseInt(prompt('Inserire il limite superiore ( numeri da 0 a 9 ) : '));
+
+const outputIndex = []; // array creato in base a quali elementi sono individuati dai parametri da prompt
+
+// uso forEach per scorrere l'array in base al valore dei parametri inseriti dai prompt. I parametri del foreach saranno element ( l'oggetto puntato di volta in volta e index, che rappresenta l'indice dell'elemento che viene di volta in volta preso in considerazione dal ciclo )
+
+nameIndex.forEach((element, index) => {
+
+    // condizione, uso l'operatore AND, l'index deve essere sia  maggiore uguale di limiteInf che minore o uguale di limiteSup
+    if(limiteInf <= index && index >= limiteSup){
+        outputIndex.push(element); // fa una push inserendo l'intero oggetto puntato nell'array outputIndex
+    }
+
+});
