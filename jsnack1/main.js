11dc56347e916ed3966b29c187bec9adf0f7e5ec
FrancescoDale <fd060862@gmail.com>
2020-11-05 20:45:57 +0100

    array con la lista delle biciclette

---
+++ jsnack1/main.js
@@ -0,0 +1,34 @@
+// array che contiene i vari oggetti bicicletta
+
+const bikeIndex = [
+
+    {
+        'model':'type0',
+        'weight': 6,
+    },
+    {
+        'model' : 'type1',
+        'weight' : 7,
+    },
+    {
+        'model' : 'type2',
+        'weight' : 5.2,
+    },
+    {
+        'model' : 'type3',
+        'weight' : 6.9,
+    },
+    {
+        'model' : 'type4',
+        'weight' : 8.1,
+    },
+    {
+        'model' : 'type5',
+        'weight' : 5.1,
+    }
+
+];
+
+// test visualizzazione array
+
+console.log(bikeIndex);
